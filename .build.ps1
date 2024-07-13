7ca36a07c8eda628c5e3a344fc5195b988c905f9
Dan Radu <radudanielro@yahoo.com>
2024-07-13 22:25:04 +1000

    Updated to v0.53.0

--- .build.ps1
+++ .build.ps1
@@ -1,11 +1,13 @@
 # Use the following command to build the image locally:
+# versions: 0.21.0, 0.34.0, 0.41.0, 0.53.0
 
-$tag='0.34.0'
-docker build -t trivy:${tag} -f Dockerfile.ubuntu .
+$tag='0.53.0'
+docker pull aquasec/trivy:${tag}
+docker build -t trivy:${tag} --build-arg VERSION=${tag} -f Dockerfile.ubuntu .
 
 # Tag the image and push to docker hub:
 
-$tag='0.34.0'
+$tag='0.53.0'
 $image='trivy'
 $registry='docker.io'
 $img="${image}:${tag}"
